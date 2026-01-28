1267a400877f7a28ad39f8ba0c9109898ac046d5
Anas Khan <anskhan67@gmail.com>
2026-01-28 14:34:11 +0500

    Initial commit

---
+++ Python/5.DuckTyping.py
@@ -0,0 +1,27 @@
+# Duck Typing = Another way of achieving Polymorphism besides Inheritance
+# Object must have the minimum necassary attributes/methods 
+# If it looks like a  duck and quacks like a duck it is a duck
+
+# class Animal:
+#     alive=True
+
+# class Dog(Animal):
+#     def speak(self):
+#         print("WOOF")
+
+# class Cat(Animal):
+#     def speak(self):
+#         print("Meow")
+
+# class Car(Animal):
+#     #def horn()
+#     alive=False
+#     def speak(self):
+#         print("Honk")  # changed horn to speak attribute
+
+# objects=[Dog(),Cat(),Car()]
+
+# for object in objects:
+#     object.speak()
+#     print(object.alive)
+
