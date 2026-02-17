ee75e426809176ac9103c0a3a132d832f76a4544
Ans Aman Ullah <anskhan67@gmail.com>
2026-02-17 22:32:40 +0500

    Add explanation for if __name__ == '__main__' usage
    Added comments explaining the purpose of the if __name__ == '__main__' construct.

--- if_main.py
+++ if_main.py
@@ -4,6 +4,11 @@ def welcome():
 if __name__=="__main__":
     welcome() #function called in file which is to imported then it will print 2 times in other file
 
+# “Why use if name == 'main'?”
+
+# Answer:
+
+# To prevent automatic execution when a module is imported and ensure code runs only when executed directly.
 
 def day_(day):
     match day:
